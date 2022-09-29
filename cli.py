784e70521e9cab5a04b2fa84903cbf335daf67bf
vince nguyen <vincehnguyen@ucsb.edu>
2022-09-28 23:23:31 -0700

    Added cli alias to command line

--- cli.py
+++ cli.py
@@ -4,13 +4,14 @@
 #cli version
 #cli gen build run
 
+import os
 import sys
 import subprocess
 
 TOOLS_DIR = "tools"
 
 def runCommand(cmd):
-    subprocess.call("python3 {}/{}.py".format(TOOLS_DIR, cmd))
+    subprocess.call(["python3", "{}/{}/{}.py".format(".", TOOLS_DIR, cmd)])
 
 for cmd in range(1, len(sys.argv)):
     cmd = sys.argv[1]
